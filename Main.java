334207a37268adaa58e76ad37d76d99e06fb7e57
Vi F <129888015+nekrochan@users.noreply.github.com>
2025-06-01 16:02:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
